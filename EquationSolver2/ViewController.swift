3c218ada90b26f867a4c6a89008ba0c85d5d0994
cmanolis9275 <cmanolis9275@bg-mmcte-17.d214.org>
2016-05-11 08:37:57 -0500

    Initial Commit

---
+++ EquationSolver2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EquationSolver2
+//
+//  Created by Christos Manolis on 5/11/16.
+//  Copyright © 2016 Christos Manolis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
